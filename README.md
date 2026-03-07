3f1b1b107f4354ada96eeb751b08fab3dfccc2e0
itsZling <micahgo425@gmail.com>
2026-03-06 22:28:07 -0500

    temp

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Installations:
   - pip install django-sass-processor
   - pip install psycopg2-binary
-  - pip install pillow  
+  - pip install pillow
+  - pip install dj-database-url
--- techhack/asgi.py
+++ techhack/asgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import home.routing # Import your app's routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techhack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    # Handle standard HTTP requests
+    "http": get_asgi_application(),
+    
+    # Handle WebSocket connections
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            game_app.routing.websocket_urlpatterns
+        )
+    ),
+})
