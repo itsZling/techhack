f0934a61f050daaf461681c2d032978b30a45706
itsZling <micahgo425@gmail.com>
2026-03-07 13:52:17 -0500

    Fix URL wildcard routing

--- lobby/urls.py
+++ lobby/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:lobby_id>/', views.index, name='lobby.index'),
     path('game/', views.game, name='game.index'),
-    path('results/', views.results, name='results')
+    path('results/', views.results, name='results'),
+    
+    # 2. Put the wildcard path LAST! 
+    # This way, it only catches actual random lobby codes.
+    path('<str:lobby_id>/', views.index, name='lobby.index'), 
 ]
