a70847a7be0e67b966869b85fdbcdd30b0a64503
itsZling <micahgo425@gmail.com>
2026-03-07 20:37:18 -0500

    changing to youtube pt1

--- lobby/views.py
+++ lobby/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Make sure all three of your Spotify tools are imported!
-from .spotify_utils import get_random_song_from_playlist, get_random_song_by_artist, get_random_song_by_genre
+from .youtube_utils import get_random_video_from_playlist
 
 def index(request, lobby_id):
     return render(request, 'lobby/index.html', {'lobby_id': lobby_id})
@@ -14,38 +14,30 @@ def game(request):
     detail = request.GET.get('detail', 'none')
     lobby_id = request.GET.get('lobby_id', 'unknown')
 
-    song_url, song_cover, song_name, song_artist = "", "", "", ""
+    video_id, song_cover, song_name, song_artist = "", "", "", ""
     error_message = ""
-
-    # Fetch the song based on the game mode
+    
     song_data = None
-    if mode == 'spotify':
-        song_data = get_random_song_from_playlist(detail)
-    elif mode == 'genre':
-        song_data = get_random_song_by_genre(detail)
-    elif mode == 'artist':
-        song_data = get_random_song_by_artist(detail)
+    if mode == 'spotify': # You can keep the 'spotify' name or rename it to 'playlist' in your HTML
+        song_data = get_random_video_from_playlist(detail)
         
-    # Check if song_data exists AND it does NOT contain our custom error message
-    if song_data and 'error' not in song_data:
-        song_url = song_data['preview_url']
-        song_cover = song_data['cover_art']
-        song_name = song_data['name']
-        song_artist = song_data['artist']
-    else:
-        # If it returned a specific error from spotify_utils, use that.
-        # Otherwise, fall back to a generic error message.
-        if song_data and 'error' in song_data:
-            error_message = song_data['error']
+        if song_data and 'error' not in song_data:
+            video_id = song_data['video_id']
+            song_cover = song_data['cover_art']
+            song_name = song_data['name']
+            song_artist = song_data['artist']
         else:
-            error_message = f"Spotify could not find any playable 30-second previews for this {mode}."
-
+            if song_data and 'error' in song_data:
+                error_message = song_data['error']
+            else:
+                error_message = f"Could not find any playable videos for this {mode}."
+   
     context = {
         'mode': mode,
         'rounds': rounds,
         'detail': detail,
         'lobby_id': lobby_id,
-        'song_url': song_url,
+        'video_id': video_id, # Passed to the template for the YouTube player
         'song_cover': song_cover,
         'song_name': song_name,
         'song_artist': song_artist,
@@ -56,5 +48,4 @@ def game(request):
 
 @login_required
 def results(request):
-    # A basic placeholder view so your urls.py doesn't throw an error!
     return render(request, 'lobby/results.html')
