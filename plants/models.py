0f21028908790bb66e0a287c1304cf83b5f9f099
Christopher Leonard <christophergleonardjr@gmail.com>
2026-03-07 21:52:02 -0500

    added logic elements to be completed later for plant functionality

--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserPlant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plant_name = models.CharField(max_length=100)
+    experience = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'plant_name')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.plant_name} ({self.experience} xp)"
--- plants/views.py
+++ plants/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import UserPlant
+from .utils import to_artist_key, get_plant_for_artist
 # Create your views here.
 
 @login_required
 def index(request):
     return render(request, 'plants/index.html')
+
+def results(request):
+    artist_key = "" #to_artist_key(artist_name), wherever artist_name comes from
+    score = 0  # calculate score based on game results
+
+    plant_name = get_plant_for_artist(artist_key)
+    if plant_name:
+        record, created = UserPlant.objects.get_or_create(
+            user=request.user,
+            plant_name=plant_name,
+            defaults={"experience": 0}
+        )
+        record.experience += score
+        record.save()
+
+    return render(request, 'lobby/results.html')
